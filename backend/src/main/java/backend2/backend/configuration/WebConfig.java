35c554d82119e19a53c9a6f0eeaf7b4b6c3c6db0
ArvidUtas <arvidu@hotmail.com>
2025-08-22 13:42:38 +0200

    Add WebConfig.java to stop CORS conflict

---
+++ backend/src/main/java/backend2/backend/configuration/WebConfig.java
@@ -0,0 +1,22 @@
+package backend2.backend.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// This ensures browsers doesn't block API requests because of Cross Origin Resourse Sharing
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("*");
+            }
+        };
+    }
+}
