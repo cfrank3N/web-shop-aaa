8a6abec95bfb661f118271469440b404c64d25df
Adam Frank <adamcecarfrank@gmail.com>
2025-08-27 18:45:34 +0200

    Removed unused imports

--- backend/src/main/java/backend2/backend/configuration/WebSecurityConfig.java
+++ backend/src/main/java/backend2/backend/configuration/WebSecurityConfig.java
@@ -1,22 +1,18 @@
 package backend2.backend.configuration;
 
-import backend2.backend.authentication.AuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-//This is needed for browsers to allow requests from frontend to backend.
 @Configuration
 @EnableWebSecurity
 @Order(2)
