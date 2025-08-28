547351535963ae99c9344d38195104828c526d2f
Gaurgle <justwannagroove@gmail.com>
2025-08-28 19:10:38 +0200

    .

--- backend/src/main/java/backend2/backend/dto/RegisterUser.java
+++ backend/src/main/java/backend2/backend/dto/RegisterUser.java
@@ -1,4 +1,21 @@
 package backend2.backend.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
 public class RegisterUser {
+
+    @Email(message = "Enter a valid emailadress")
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    @Size(min = 8, message = "password must contain at least 8 characters")
+    private String password;
+
+    @NotBlank
+    private String confirmPassword;
 }
