00e7c51c3872c4678f929f8ddc3da1d07091591d
Adam Frank <adamcecarfrank@gmail.com>
2025-08-29 13:24:54 +0200

    Added DTO's and mappers, still have issues with WebSecurityConfig. Added Lombok to build.gradle locally

---
+++ backend/src/main/java/backend2/backend/mappers/CustomerMapper.java
@@ -0,0 +1,19 @@
+package backend2.backend.mappers;
+
+import backend2.backend.dtos.CustomerDTO;
+import backend2.backend.entities.Authority;
+import backend2.backend.entities.Customer;
+
+public class CustomerMapper {
+    public static CustomerDTO customerToCustomerDTODetailed(Customer c) {
+        return CustomerDTO.builder()
+                .id(c.getId())
+                .username(c.getUsername())
+                .password(c.getPassword())
+                .authorities(c.getAuthorities()
+                        .stream()
+                        .map(Authority::getAuthority)
+                        .toList())
+                .build();
+    }
+}
---
+++ backend/src/main/java/backend2/backend/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package backend2.backend.repository;
+
+import backend2.backend.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+}
