a37c07bd2036e9040625b6b0a5de53bc2a3a80c2
ArvidUtas <arvidu@hotmail.com>
2025-08-29 11:58:41 +0200

    Move fetchAndSaveAllProducts to FakeStoreClient and schedule it to run on startup and at 0:30 every day

--- backend/src/main/java/backend2/backend/BackendApplication.java
+++ backend/src/main/java/backend2/backend/BackendApplication.java
@@ -1,17 +1,10 @@
 package backend2.backend;
 
-import backend2.backend.service.ProductService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendApplication {
-    private final ProductService productService;
-
-    public BackendApplication(ProductService productService) {
-        this.productService = productService;
-        productService.fetchAndSaveAllProducts();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
