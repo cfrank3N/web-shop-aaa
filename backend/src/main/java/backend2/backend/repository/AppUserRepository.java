a4825d84e5e223d5b71110678aaf4bdb9aa1b6aa
ArvidUtas <arvidu@hotmail.com>
2025-09-02 15:16:50 +0200

    Commit temp files to merge

---
+++ backend/src/main/java/backend2/backend/repository/AppUserRepository.java
@@ -0,0 +1,9 @@
+package backend2.backend.repository;
+
+import backend2.backend.entities.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppUserRepository extends JpaRepository<AppUser, Integer> {
+}
