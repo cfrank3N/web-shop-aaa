23ae337b8a4bf46be6b58847bb06482536b8ad67
ArvidUtas <arvidu@hotmail.com>
2025-09-02 08:40:37 +0200

    Built saveOrder in controller and service

--- backend/src/main/java/backend2/backend/controller/OrderController.java
+++ backend/src/main/java/backend2/backend/controller/OrderController.java
@@ -1,7 +1,25 @@
 package backend2.backend.controller;
 
+import backend2.backend.entities.Order;
+import backend2.backend.service.OrderService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class OrderController {
+    private final OrderService service;
+
+    public OrderController(OrderService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/order")
+    public ResponseEntity<String> saveOrder(@RequestBody Order order){
+        return service.saveOrder(order);
+    }
+
 }
