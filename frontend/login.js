510d822fa625fc070cf1f9a4022e75872967552b
Adam Frank <adamcecarfrank@gmail.com>
2025-09-02 20:28:23 +0200

    Added invalid feedback on the form if wrong credentials are submitted from user. Added comments

--- frontend/login.js
+++ frontend/login.js
@@ -9,13 +9,14 @@ document.addEventListener("DOMContentLoaded", () => {
 async function login(e) {
   e.preventDefault();
 
-  //Fetch provided credentials from frontend form  
-  const usernameFromForm = document.querySelector("#username").value;
-  const passwordFromForm = document.querySelector("#password").value;
+  //Fetch provided credentials from frontend form
+  const invalidElement = document.getElementById("wrongCredentials");
+  const passWord = document.getElementById("password");
+  const userName = document.getElementById("username");
 
   //Makes a call to the API to check if user is in DB, i.e a registered user, and
   //also checks if the provided username and password matches with a registered user.
-   
+
   try {
     const response = await fetch("http://localhost:8080/auth/login", {
       method: "POST",
@@ -23,29 +24,48 @@ async function login(e) {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        username: usernameFromForm,
-        password: passwordFromForm,
+        username: userName.value,
+        password: passWord.value
       }),
     });
 
-    //If provided username and password doesn't match 
+    //If provided username and password doesn't match
     //API sends back a 400 Bad request
-    
-    //TODO: add message in form/on page that username or password doesn't match 
+
+    //TODO: add message in form/on page that username or password doesn't match
     if (!response.ok) {
       const errorMessage = await response.text();
+
+      invalidElement.textContent = errorMessage;
+
+      //Check if element has is-invalid already, otherwise add it for user clarity
+      if (
+        !passWord.classList.contains("is-invalid") &&
+        !userName.classList.contains("is-invalid")
+      ) {
+        passWord.classList.add("is-invalid");
+        userName.classList.add("is-invalid");
+      }
       console.log(errorMessage);
       return;
     }
 
-    //Saves the provided authenticated JWT from the API 
-    //in localStorage for future requests
+    //Simply removes the is-invalid on the form elements to not
+    //have it linger until next time they need to use login
+
+      passWord.classList.remove("is-invalid");
+      userName.classList.remove("is-invalid");
+
+
+    //Saves the provided authenticated JWT from the API
+    //in sessionStorage for future auth requests to the API
     const jwt = await response.text();
-    localStorage.setItem("jwt", jwt);
+    sessionStorage.setItem("jwt", jwt);
 
     //Relocates the user to the frontpage after successful login
     window.location.href = "/index.html";
-    alert("Successful login! Provided JWT: " + jwt);
+    //TODO: Remove this alert when done with implementation
+    alert("Successful login!");
   } catch (err) {
     console.log("Something went wrong");
   }
