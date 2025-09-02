594fe989af3754bc8c0b0b8ef171794528748b27
Adam Frank <adamcecarfrank@gmail.com>
2025-09-02 18:00:05 +0200

    Added relevant comments

--- frontend/login.js
+++ frontend/login.js
@@ -9,9 +9,13 @@ document.addEventListener("DOMContentLoaded", () => {
 async function login(e) {
   e.preventDefault();
 
+  //Fetch provided credentials from frontend form  
   const usernameFromForm = document.querySelector("#username").value;
   const passwordFromForm = document.querySelector("#password").value;
 
+  //Makes a call to the API to check if user is in DB, i.e a registered user, and
+  //also checks if the provided username and password matches with a registered user.
+   
   try {
     const response = await fetch("http://localhost:8080/auth/login", {
       method: "POST",
@@ -24,17 +28,24 @@ async function login(e) {
       }),
     });
 
+    //If provided username and password doesn't match 
+    //API sends back a 400 Bad request
+    
+    //TODO: add message in form/on page that username or password doesn't match 
     if (!response.ok) {
       const errorMessage = await response.text();
       console.log(errorMessage);
       return;
     }
 
+    //Saves the provided authenticated JWT from the API 
+    //in localStorage for future requests
     const jwt = await response.text();
     localStorage.setItem("jwt", jwt);
-    console.log(jwt);
 
+    //Relocates the user to the frontpage after successful login
     window.location.href = "/index.html";
+    alert("Successful login! Provided JWT: " + jwt);
   } catch (err) {
     console.log("Something went wrong");
   }
