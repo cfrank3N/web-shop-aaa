8a41e07a6fc012e6f2ddc573dc98f176d78ee88d
ArvidUtas <arvidu@hotmail.com>
2025-09-02 08:29:03 +0200

    Set up classes and rough entity

---
+++ backend/src/main/java/backend2/backend/entities/Order.java
@@ -0,0 +1,26 @@
+package backend2.backend.entities;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customerId;
+
+    @ElementCollection
+    private List<Integer> orderedProductIds;
+
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+}
