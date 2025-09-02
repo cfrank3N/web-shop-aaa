6fbabe7a0bf95cf66735e6a2c3c95d05658fce7e
ArvidUtas <arvidu@hotmail.com>
2025-09-02 08:29:03 +0200

    Set up classes and rough entity

---
+++ backend/src/main/java/backend2/backend/repository/OrdersRepository.java
@@ -0,0 +1,9 @@
+package backend2.backend.repository;
+
+import backend2.backend.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrdersRepository extends JpaRepository<Order, Long> {
+}
