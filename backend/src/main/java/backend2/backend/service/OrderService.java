deda0df7ed8a110b9af75a4fe7a64c3b8c5b6c09
ArvidUtas <arvidu@hotmail.com>
2025-09-02 08:40:37 +0200

    Built saveOrder in controller and service

--- backend/src/main/java/backend2/backend/service/OrderService.java
+++ backend/src/main/java/backend2/backend/service/OrderService.java
@@ -1,7 +1,26 @@
 package backend2.backend.service;
 
+import backend2.backend.entities.Order;
+import backend2.backend.repository.OrdersRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
+    private final OrdersRepository repo;
+
+    public OrderService(OrdersRepository repo) {
+        this.repo = repo;
+    }
+
+    public ResponseEntity<String> saveOrder(Order order){
+        try {
+            repo.save(order);
+            return ResponseEntity.ok("Order placed");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Failed to place order " + e.getMessage());
+        }
+    }
 }
