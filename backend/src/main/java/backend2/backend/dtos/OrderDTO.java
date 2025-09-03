b4e386770fe08a8c4ae450c69c94753f7daa5f41
ArvidUtas <arvidu@hotmail.com>
2025-09-03 15:02:23 +0200

    FE + BE: Refactored Place Order, work. Fixed multiplicity of products TODO set user id

--- backend/src/main/java/backend2/backend/dtos/OrderDTO.java
+++ backend/src/main/java/backend2/backend/dtos/OrderDTO.java
@@ -1,30 +1,22 @@
 package backend2.backend.dtos;
 
-import java.util.List;
+import java.util.Map;
 
 public class OrderDTO {
 
     private final int appUserId;
-    private final List<Integer> orderedProductIds;
+    private final Map<Integer, Integer> productIdAndQty;
 
-    public OrderDTO(int appUserId, List<Integer> orderedProductIds) {
+    public OrderDTO(int appUserId, Map<Integer, Integer> productIdAndQty) {
         this.appUserId = appUserId;
-        this.orderedProductIds = orderedProductIds;
+        this.productIdAndQty = productIdAndQty;
     }
 
     public int getAppUserId() {
         return appUserId;
     }
 
-    public List<Integer> getOrderedProductIds() {
-        return orderedProductIds;
-    }
-
-    @Override
-    public String toString() {
-        return "OrderDTO{" +
-                "appUserId=" + appUserId +
-                ", orderedProductIds=" + orderedProductIds +
-                '}';
+    public Map<Integer, Integer> getProductIdAndQty() {
+        return productIdAndQty;
     }
 }
