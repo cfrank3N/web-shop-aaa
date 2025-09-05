83e225a687e6dc6771e7af3256eeeb31576e8e5a
Gaurgle <justwannagroove@gmail.com>
2025-09-05 19:50:17 +0200

    integration test 1.0, fixed

--- backend/src/test/java/backend2/backend/FakeStoreAPITest.java
+++ backend/src/test/java/backend2/backend/FakeStoreAPITest.java
@@ -2,9 +2,9 @@ package backend2.backend;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -14,38 +14,45 @@ import java.time.Duration;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class FakeStoreAPITest {
+    HttpClient client = HttpClient.newBuilder()
+            .connectTimeout(Duration.ofSeconds(10))
+            .build();
+
+    HttpRequest requestResponse = HttpRequest.newBuilder()
+            .uri(URI.create("https://fakestoreapi.com/"))
+            .timeout(Duration.ofSeconds(10))
+            .GET()
+            .build();
+
+    HttpRequest requestFirstProduct = HttpRequest.newBuilder()
+            .uri(URI.create("https://fakestoreapi.com/products/1"))
+            .timeout(Duration.ofSeconds(10))
+            .GET()
+            .build();
 
     @Test
-    void dummyTest() {
+    void sanityCheck() {
         System.out.println("testing");
     }
 
-    record Rating(Double rate, Integer count) {
-    }
-
-    record Product(Long id, String title, Double price, String description, String category,
-                   String imageURL, Rating rating) {
+    @Test
+    void connectionTest() throws IOException, InterruptedException {
+        HttpResponse<String> response = client.send(requestResponse, HttpResponse.BodyHandlers.ofString());
+        assertThat(response.statusCode()).isEqualTo(200);
+        assertThat(response.statusCode()).isNotEqualTo(400);
     }
 
     @Test
     void productById_HasAllAttributes() throws Exception {
 
-        HttpClient client = HttpClient.newBuilder()
-                .connectTimeout(Duration.ofSeconds(10))
-                .build();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://fakestoreapi.com/products/1"))
-                .timeout(Duration.ofSeconds(10))
-                .GET()
-                .build();
-
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        HttpResponse<String> response = client.send(requestFirstProduct, HttpResponse.BodyHandlers.ofString());
 
         assertThat(response.statusCode()).isEqualTo(200);
 
         ObjectMapper mapper = new ObjectMapper();
         JsonNode root = mapper.readTree(response.body());
 
+        assertThat(root.size()).isGreaterThan(0);
         assertThat(root.get("id")).as("id").isNotNull();
         assertThat(root.path("title").asText(null)).as("title").isNotBlank();
         assertThat(root.get("price")).as("price").isNotNull();
