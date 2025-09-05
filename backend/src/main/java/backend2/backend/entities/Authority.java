8788a1b98eee2cb365da98beb64e009d273d1b4d
Adam Frank <adamcecarfrank@gmail.com>
2025-09-05 16:16:25 +0200

    Forgot to commit often... Added regisdtration form and script in frontend. Changed the logic inside the register method in the api

--- backend/src/main/java/backend2/backend/entities/Authority.java
+++ backend/src/main/java/backend2/backend/entities/Authority.java
@@ -37,4 +37,11 @@ public class Authority {
     public void setAuthority(String authority) {
         this.authority = authority;
     }
+
+    @Override
+    public String toString() {
+        return "Authority{" +
+                "authority='" + authority + '\'' +
+                '}';
+    }
 }
